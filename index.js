211512ae40aa77d4785e7d5038faca70cb760a58
chillbuilds <will@chillbuilds.com>
2020-06-24 12:19:26 -0600

    message sent to channel with @here notification

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const { WebClient } = require('@slack/web-api');
- 
-// Read a token from the environment variables
-const token = process.env.SLACK_TOKEN;
- 
-// Initialize
-const web = new WebClient(token);
+const { IncomingWebhook } = require('@slack/webhook')
+const url = ''
+const webhook = new IncomingWebhook(url)
 
-// Given some known conversation ID (representing a public channel, private channel, DM or group DM)
-const conversationId = '...';
- 
-(async () => {
- 
-  // Post a message to the channel, and await the result.
-  // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-  const result = await web.chat.postMessage({
-    text: 'Hello world!',
-    channel: conversationId,
-  });
- 
-  // The result contains an identifier for the message, `ts`.
-  console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
-})();
-
-console.log(web)
+webhook.send({
+  text: '<!here> notification',
+})
